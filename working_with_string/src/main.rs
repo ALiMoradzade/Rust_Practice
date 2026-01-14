2cca2024e0166058a1444312bbfba804e4f4065d
ALi Moradzade <alimooradzade@gmail.com>
2026-01-14 12:58:00 +0330

    added string concatenation

--- working_with_string/src/main.rs
+++ working_with_string/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+    let mut s = String::new();
+    
+    // s = s1; // value is moved
+    // s = s1.as_str().parse().unwrap(); // gets str and converts it to String
+    
+    // s = s1 + " " + s2.as_str(); // s1 is moved and did String + str
+    s = s + &s1 + &s2; // s1 is moved and did String + str
+    
+    println!("{}", s);
 }
